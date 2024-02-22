0b4f985e9caf959edda803d8a946ad5052e33b90
Khalid <86178391+codemaestro908@users.noreply.github.com>
2024-02-22 11:45:56 +0530

    header

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="col-lg-12 col-md-12 col-sm-12 col-12 py-3 text-center bg-warning border">
+		<h2>Human Face Detection and recognition</h2>
+		<h6>Identifiying Missing/Unknown Children</h6>
+		<h6 class="p-2 my-2 bg-dark text-white">Developed By - Mohin Ahmad(1705213023), Jay Prakash(1805213906), Mohd. Khalid(1805213908)</h6>
+		<h6 class="links mt-3">
+			<router-link to="/" class="links">Home</router-link> &nbsp; &nbsp;
+			<router-link to="/admin" class="links">Admin</router-link> &nbsp; &nbsp;
+			<router-link to="/about" class="links">About</router-link> &nbsp; &nbsp;
+		</h6>
+	</div>
+</template>
+
+<style scoped>
+	.links{
+		text-decoration: none;
+	}
+</style>
